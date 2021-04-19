abc526ab8a5e9d26283e05aa350c07ad47b1b164
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2021-04-19 16:23:06 +0200

    update for add embed

--- main.js
+++ main.js
@@ -1,37 +1,76 @@
 const axios = require('axios');
+const dotenv = require('dotenv');
+dotenv.config();
 
-axios.get(`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/releases/latest`)
+axios.get(`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/releases/tags/v6.0.0`)
 .then(function (response) {
+  // var format = []
+
+  // var test = response['data']['body'].split('\r\n');
+  // var buffer_title = [];
+  // var buffer_content = [];
+  // var buffer_index = [1];
+  // buffer_title.push(test[0]);
+  // for(i=0;i < test.length;i++){
+  //   if(test[i] == ""){
+  //     buffer_title.push(test[i+1]);
+  //     buffer_index.push(i+2);
+  //   }
+  // }
+  // buffer_index.push(test.length);
+  // for(i=0;i < buffer_index.length;i++){
+  //   myChunk = test.slice(buffer_index[i], buffer_index[i + 1]);
+  //   if(i != (buffer_index.length - 2)){
+  //     myChunk.pop();
+  //     myChunk.pop();
+  //   }
+  //   buffer_content.push(myChunk);
+  // }
+  // for(i=0;i < buffer_content.length;i++){
+  //   var waw = '';
+  //   for(r=0;r < buffer_content[i].length;r++){
+  //     waw = buffer_content[i][r] + '\n';
+  //     if(buffer_content[i].length){
+  //       format.push({"name": buffer_title[i], "value": waw});
+  //     }
+  //   }
+  // }
+  // console.log(test);
+  // console.log(buffer_title);
+  // console.log(buffer_content);
+  // console.log(buffer_index);
+  // console.log(format);
+
   axios.post(`${process.env.DISCORD_WEBHOOK}`, {
     username: process.env.DISCORD_USERNAME,
     avatar_url: process.env.DISCORD_AVATAR,
-    content: `NEW VERSION: ${response['data']['tag_name']} ${response['data']['body']}`,
+    content: null,
     embeds: [
       {
-        "title": "New Version",
-        "description": "So WAW",
-        "url": "https://raw.githubusercontent.com/LeGitHubDeTai/github-to-discord",
-        "color": 5814783,
+        "title": `${process.env.MESSAGE_TITLE} ${response['data']['tag_name']}`,
+        "description": `${process.env.MESSAGE_DESCRIPTION}`,
+        "url": `${process.env.MESSAGE_URL}`,
+        "color": process.env.MESSAGE_COLOR,
         "fields": [
           {
-            "name": "Add",
-            "value": "- cc\n- cc\n- cc\n- cc"
+            "name": "ChangeLog",
+            "value": `${name(response['data']['body'], '##', '')}`
           }
         ],
         "author": {
           "name": "Tai Studio",
-          "url": "https://raw.githubusercontent.com/LeGitHubDeTai/github-to-discord",
-          "icon_url": "https://raw.githubusercontent.com/LeGitHubDeTai/github-to-discord/main/bot.jpg"
+          "url": "https://tai-studio.ml",
+          "icon_url": "https://tai-studio.ml/img/icons/Tai_Studio_discord.png"
         },
         "footer": {
           "text": "- Tai Studio © 2021 -",
-          "icon_url": "https://raw.githubusercontent.com/LeGitHubDeTai/github-to-discord/main/bot.jpg"
+          "icon_url": "https://tai-studio.ml/img/icons/Tai_Studio_discord.png"
         },
         "image": {
-          "url": "https://raw.githubusercontent.com/LeGitHubDeTai/github-to-discord/main/bot.jpg"
+          "url": "https://raw.githubusercontent.com/LeGitHubDeTai/github-to-discord/main/icon.png"
         },
         "thumbnail": {
-          "url": "https://raw.githubusercontent.com/LeGitHubDeTai/github-to-discord/main/bot.jpg"
+          "url": `https://raw.githubusercontent.com/${process.env.GITHUB_REPOSITORY}/icon.png`
         }
       }
     ]
@@ -46,3 +85,9 @@ axios.get(`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/release
 .catch(function (error) {
   console.log(error);
 });
+
+function name(str,replaceWhat,replaceTo){
+    replaceWhat = replaceWhat.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+    var re = new RegExp(replaceWhat, 'g');
+    return str.replace(re,replaceTo);
+}
