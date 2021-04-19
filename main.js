cf2138ea73fa9ad418d5d299d1840cf783854248
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2021-04-19 13:01:16 +0200

    update

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const axios = require('axios');
 
-axios.get(`https://api.github.com/repos/${process.env.GITHUB_NAME}/${process.env.REPO_NAME}/releases/latest`)
+axios.get(`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/releases/latest`)
 .then(function (response) {
-  axios.post(`${process.env.WEBHOOK}`, {
-    username: process.env.USERNAME,
-    avatar_url: process.env.AVATAR,
+  axios.post(`${process.env.DISCORD_WEBHOOK}`, {
+    username: process.env.DISCORD_USERNAME,
+    avatar_url: process.env.DISCORD_AVATAR,
     content: `NEW VERSION: ${response['data']['tag_name']} ${response['data']['body']}`
   })
   .then(function (response) {
